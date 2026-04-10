1866821a67595a6c0e0b192d6b941ebbe23135ef
yaduvanshijitin <jitinrao95@gmail.com>
2026-04-10 16:45:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,57 @@
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name = "jenkins-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30007
+    to_port     = 30007
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "jenkins_server" {
   ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
+
+  security_groups = [aws_security_group.jenkins_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              apt install openjdk-11-jdk -y
+              apt install git -y
+              usermod -aG docker ubuntu
+              systemctl start docker
+              systemctl enable docker
+              wget -O /usr/share/keyrings/jenkins-keyring.asc \
+              https://pkg.jenkins.io/debian-stable/jenkins.io.key
+              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+              https://pkg.jenkins.io/debian-stable binary/ | tee \
+              /etc/apt/sources.list.d/jenkins.list > /dev/null
+              apt update -y
+              apt install jenkins -y
+              systemctl start jenkins
+              systemctl enable jenkins
+              EOF
 
   tags = {
     Name = "jenkins-server"
